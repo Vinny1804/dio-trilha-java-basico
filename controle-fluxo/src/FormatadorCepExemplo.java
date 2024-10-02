9bbc54b58830d34b7684c0efb8dcc27489da6b69
Vinicius <vinny_aguiar@hotmail.com>
2024-10-01 22:24:07 -0300

    Correção no código

--- controle-fluxo/src/FormatadorCepExemplo.java
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -11,8 +11,6 @@ public class FormatadorCepExemplo {
     static String formatarCep(String cep) throws CepInvalidoException{
         if(cep.length() != 8)
             throw new CepInvalidoException();
-        
-          //simulando um cep formatado
           return "23.765-064";
     }
 }
